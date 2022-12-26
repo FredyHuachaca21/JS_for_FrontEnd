58ee835f9ba25662aa62e8139b27a86640673136
Fredy <fredy.huachaca21@gmail.com>
2022-12-26 17:11:01 -0500

    Import and Export

---
+++ src/bases/08-import-export.js
@@ -0,0 +1,31 @@
+import {heroes} from './data/heroes'
+
+const getHeroeByID = (id)=>{
+    return heroes.find((x)=>{
+        if(x.id === id){
+            return true;
+        }else{
+            return false;
+        } 
+    });
+}
+
+console.log(getHeroeByID(4))
+
+//Optimizando la busqueda 1ra forma
+const getHeroeByID_01 = (id) =>{
+    return heroes.find((heroes) => heroes.id === id);
+}
+console.log(getHeroeByID_01(3));
+
+
+//Optimizando la busqueda 2da forma
+const getHeroeByID_02 = (id) => heroes.find(heroes=> heroes.id === id);
+console.log(getHeroeByID_02(5));
+
+//busqueda por owner
+const getHeroeByOwner = (owner)=>{
+    return heroes.filter(x => x.owner === owner) 
+}
+
+console.log(getHeroeByOwner('Marvel'))
