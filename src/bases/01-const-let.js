bdbc61c1fa08d1ed1323ddacc0f02c44f4a5a2c5
Fredy <fredy.huachaca21@gmail.com>
2022-12-26 12:55:15 -0500

    const and let

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+
+const nombre = "Fredy";
+let apellido = "Huachaca";
+
+let valorDado = 5;
+
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if(true){
+    let valorDado = 10;
+    console.log(valorDado);
+}
+
+console.log(valorDado)
